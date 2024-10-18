144752c0809aebb3b35dfea255364f6966d791d4
oSans0 <liueric42@gmail.com>
2024-10-18 16:13:55 +0200

    Update schema3.sql

--- schema3.sql
+++ schema3.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Emprunts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     livre_id INTEGER NOT NULL,
-    date_emprunt DATE DEFAULT CURRENT_DATE,
+    date_emprunt DATE DEFAULT CURRENT_DATE, 
     date_retour DATE,
     FOREIGN KEY(user_id) REFERENCES Utilisateur(id),
     FOREIGN KEY(livre_id) REFERENCES Bibliotheque(ID_livre)
