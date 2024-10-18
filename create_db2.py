9cd43cd69207dda18a1c06ff85ed019d22316fe2
oSans0 <liueric42@gmail.com>
2024-10-18 14:50:20 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as f:
+with open('schema2.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
