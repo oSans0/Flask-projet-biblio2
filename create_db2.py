664aa9dc9d1ff233df37797260f298a3e2c5ad8f
oSans0 <liueric42@gmail.com>
2024-10-18 15:22:32 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -7,9 +7,9 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('YUI', '12345'))
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('LEROUX', '12345'))
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('MARTIN', '12345'))
+cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('YUI', 12345))
+cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('LEROUX', 12345))
+cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('MARTIN', 12345))
 
 connection.commit()
 connection.close()
