2aa725ce2e958aa94ecc83cf7d75dafd576da3af
oSans0 <liueric42@gmail.com>
2024-10-18 14:54:20 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -7,14 +7,14 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUPONT', 'Emilie'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('TREMBLAY', 'Antoine'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LAMBERT', 'Sarah'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGNON', 'Nicolas'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('DUPONT', 'Emilie'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('LEROUX', 'Lucas'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('MARTIN', 'Amandine'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('TREMBLAY', 'Antoine'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('LAMBERT', 'Sarah'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('GAGNON', 'Nicolas'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('DUBOIS', 'Charlotte'))
+cur.execute("INSERT INTO clients (nom, prenom) VALUES (?, ?)",('LEFEVRE', 'Thomas'))
 
 connection.commit()
 connection.close()
