173b25b9e69e90bffa9c4d2b7839b6c80a509dfd
oSans0 <liueric42@gmail.com>
2024-10-18 14:28:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,6 +39,17 @@ def enregistrer_livre():
 
     return render_template('formulaire_livre.html')  # Affiche le formulaire pour enregistrer un livre
 
+@app.route('/fiche_livre/<string:post_nom>')
+def Readnom(post_nom):
+    if not est_authentifie_nom():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Bibliotheque WHERE nom = ?', (post_nom,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 @app.route('/emprunter_livre', methods=['GET', 'POST'])
 def emprunter_livre():
     if request.method == 'POST':
