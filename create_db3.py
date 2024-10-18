51c3e0b2470f69b868e96b3e40d0814317902796
oSans0 <liueric42@gmail.com>
2024-10-18 15:46:30 +0200

    Update create_db3.py

--- create_db3.py
+++ create_db3.py
@@ -5,11 +5,5 @@ connection = sqlite3.connect('database.db')
 with open('schema3.sql') as f:
     connection.executescript(f.read())
 
-cur = connection.cursor()
-
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('YUI', 12345))
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('LEROUX', 12345))
-cur.execute("INSERT INTO Utilisateur (username, password) VALUES (?, ?)",('MARTIN', 12345))
-
 connection.commit()
 connection.close()
